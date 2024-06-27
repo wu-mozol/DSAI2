c3849d496cce7112cc1f1d5f2ca6514881002d8a
h11945878 <h11945878@s.wu.ac.at>
2024-06-27 03:07:05 +0200

    Quickfix of git artifacts

---
+++ data/truth_social_data/readme.txt
@@ -0,0 +1,151 @@
+Data files structure
+----------------------
+Driving this dataset is a network of users connected to their posts ("Truths"). The Truths are then
+connected to other entities (hashtags, quotes, replies, media, external_urls) that give more context about the information contained in each Truth.
+All files are tab-separated and encoded using UTF-8.
+
+A breakdown of the number of data points for each file:
+users.tsv:  454,458
+follows.tsv: 4,002,115
+truths.tsv: 845,060
+quotes.tsv: 10,508
+replies.tsv: 506,276
+media.tsv: 184,884
+hashtags.tsv: 21,599
+external_urls.tsv: 173,947
+truth_hashtag_edges.tsv: 213,295
+truth_media_edges.tsv: 257,500
+truth_external_url_edges.tsv: 252,877
+truth_user_tag_edges.tsv: 145,234
+
+--------------------------------------------
+Terms to Know
+--------------------------------------------
+Truth - Truth Social's term for a post. Near-equivalent to a Tweet.
+ReTruth - Truth Social's equivalent to a retweet. When a user ReTruths a Truth, that original Truth appears on their posts, but the ReTruth cannot be
+liked, ReTruthed, or replied to (one can only perform these actions on the original Truth) 
+
+--------------------------------------------
+users.tsv file structure
+--------------------------------------------
+|id|timestamp|time_scraped|username|follower_count|following_count|profile_url|finished_follower_scrape|finished_following_scrape|finished_truth_scrape
+id - unique integer id 
+timestamp - datetime of the listed user creation date (note that Truth Social only provides the month and year a user's profile was create)
+time_scraped - datetime of when the user's profile was last scraped
+username - text of the user's username
+follower_count - int of the number of followers listed for the user
+following_count - int of the number of users the user is listed as following
+profile_url - text of the url of the user's profile
+finished_follower_scrape - boolean indicating if the user's followers have been entirely scraped at least once
+finished_following_scrape - boolean indicating if the user's following have been entirely scraped at least once
+finished_truth_scrape - boolean indicating if the user's truths have been entirely scraped at least once
+--------------------------------------------
+follows.tsv file structure
+--------------------------------------------
+id|time_scraped|follower|followed
+id - unique integer id 
+time_scraped - datetime of when the edge was added to the data
+follower - int foreign key referencing the follower user
+followed - int foreign key referencing the followed user
+--------------------------------------------
+truths.tsv file structure
+--------------------------------------------
+id|timestamp|time_scraped|is_retruth|is_reply|author|like_count|retruth_count|reply_count|text|external_id|url|truth_retruthed
+id - unique integer id 
+timestamp - datetime of when the Truth was posted
+time_scraped - datetime of when the Truth was added to the data
+is_retruth - boolean indicating whether the Truth is a ReTruth
+is_reply - boolean indicating whether the Truth is a reply
+author - int foreign key referencing the user who posted the Truth
+like_count - int of number of likes associated with Truth at time of scrape
+retruth_count - int of number of ReTruths associated with Truth at time of scrape
+text - text of truth
+external_id -  int of Truth Social's unique id associated with Truth
+url - text of url to Truth
+truth_retruthed - int foreign key to Truth current Truth is ReTruthing. If the current Truth is not a ReTruth, this field is set to -1
+--------------------------------------------
+quotes.tsv file structure
+--------------------------------------------
+id|timestamp|time_scraped|quoted_user|quoting_user|quoting_truth|quoted_truth_external_id
+id - unique integer id 
+timestamp - datetime of when the Truth was posted
+time_scraped - datetime of when the quote was added to the data
+quoted_user - int foreign key to the quoted user
+quoting_user - int foreign key to the quoting user
+quoting_truth - int foreign key to the quoting Truth
+quoted_truth_external_id - int of the Truth Social's unique id of the quoted Truth
+--------------------------------------------
+replies.tsv file structure
+--------------------------------------------
+id|time_scraped|replying_user|replied_user
+id - unique integer id 
+time_scraped - datetime of when the reply was added to the data
+replying_user - int foreign key to the replying user
+replied_user - int foreign key to the replied user
+--------------------------------------------
+media.tsv file structure
+--------------------------------------------
+id|media_url|
+id - unique integer id 
+media_irl - text of the media's url
+--------------------------------------------
+hashtags.tsv file structure
+--------------------------------------------
+id|first_seen|hashtag|
+id - unique integer id 
+first_seen - datetime of when the hashtag was first added to the data
+hashtag - text of the url
+--------------------------------------------
+external_urls.tsv file structure
+--------------------------------------------
+id|url|
+id - unique integer id 
+url - text of the external url
+--------------------------------------------
+truth_media_edges.tsv file structure
+--------------------------------------------
+id|truth_id|media_id|
+id - unique integer id 
+truth_id - int foreign key of the referenced truth
+media_id - int foreign key of the referenced media
+--------------------------------------------
+truth_hashtag_edges.tsv file structure
+--------------------------------------------
+id|truth_id|hashtag_id|
+id - unique integer id 
+truth_id - int foreign key of the referenced truth
+hashtag_id - int foreign key of the referenced hashtag
+--------------------------------------------
+truth_external_url_edges.tsv file structure
+--------------------------------------------
+id|truth_id|url_id|
+id - unique integer id 
+truth_id - int foreign key of the referenced truth
+url_id - int foreign key of the referenced url
+--------------------------------------------
+truth_user_tag_edges.tsv file structure
+--------------------------------------------
+id|time_scraped|truth_id|user_id
+id - unique integer id 
+time_scraped - datetime of when the entry was added to the data
+truth_id - int foreign key of the referenced truth
+user_id - int foreign key of the tagged user
+
+
+--------------------------------------------
+Significant information about the Data
+--------------------------------------------
+Due to Truth Social's query limitations, not all users.tsv and truths.tsv data points were pulled directly from Truth Social.
+
+- For truths: in order to link ReTruthing Truths to their original Truths, during the data-cleaning phase,
+    if a Truth is a ReTruth, and its originally referenced Truth did not exist in the data, an entry was created for the original Truth. This entry, however, lacked the Truth's original timestamp.
+    To indicate that this information is missing, a timestamp of -1 is listed.
+- For users: in order to ensure that all follow edges reference a user entry, during the data-cleaning phase,
+    a new user entry was create if:
+    1. A newly-created Truth was linked to an author whose user entry did not yet exist;
+    2. A follow edge referenced a user whose entry did not yet exist.
+
+    Care was taken to indicate missing information in these entries. Specifically, these user entries contain a -1 in the following fields:
+    timestamp, follower_count, following_count, finished_follower_scrape, finished_following_scrape, finished_truth_scrape
+
+- When a user is scraped, the user entry's time_scraped is updated; if that user is seen again in the user queue, it will be scraped again if the time_scraped field is at least 7 days from the current date.
--- functions/functions_analysis.R
+++ functions/functions_analysis.R
@@ -1,13 +1,13 @@
 graph_main_analysis <- function(g){
-  
+
   #C Number of vertices (order)
   print('Finding graph order')
   g_order <- vcount(g)
-  
+
   #C Number of edges (size)
   print('Finding graph size')
   g_size <- ecount(g)
-  
+
   #C Degrees (List, Min, Max)
   print('Finding degree statistics')
   g_deg <- degree(g, mode = 'all')
@@ -19,7 +19,7 @@ graph_main_analysis <- function(g){
   g_out_deg <- degree(g, mode = 'out')
   g_out_deg_max <- max(g_out_deg)
   g_out_deg_min <- min(g_out_deg)
-  
+
   #C Connectivity
   print('Finding connectivity statistics')
   g_no_comp_weak <- components(g, mode = 'weak')$no
@@ -35,7 +35,7 @@ graph_main_analysis <- function(g){
   #g_vconn_underlying <- vertex_connectivity(as.undirected(g, 'mutual'))
   g_econn <- edge_connectivity(g)
   #g_econn_underlying <- edge_connectivity(as.undirected(g, 'mutual'))
-  
+
   #C Distances
   print('Finding distance statistics')
   if (g_con == 'strong') {
@@ -49,34 +49,34 @@ graph_main_analysis <- function(g){
     g_apl <- mean_distance(g, directed = TRUE, unconnected = TRUE)
     #g_apl_underlying <- mean_distance(g, directed = FALSE, unconnected = TRUE)
   }
-  
+
   #C Transitivity
   print('Finding transitivity')
   g_trans <- transitivity(g)
-  
+
   #C Highest centrality node's label
-  
+
   #C Degree centrality
   print('Finding degree centrality')
   g_deg_c <- g_deg_max
   #g_deg_c_n <- max(g_deg/g_deg_max)
   g_deg_c_label <- V(g)$label[g_deg==g_deg_c]
-  
+
   #C In-Degree Centrality
   #g_deg_c_in <- max(degree(g, mode = 'in'))
   #g_deg_c_in_n <- max(g_deg/g_deg_max)
   #g_deg_c_in_label <- V(g)$label[degree(g, mode = 'in')==g_deg_c_in]
-  
+
   #C Eigenvector centrality
   print('Finding eigenvector centrality')
   g_eigen_c <- eigen_centrality(g, directed = TRUE)$vector
   #g_eigen_c_n <- g_eigen_c/max(g_eigen_c)
   g_eigen_c_label <- V(g)$label[g_eigen_c==max(g_eigen_c)]
-  
+
   #C Underlying Eigenvector centrality
   #g_eigen_c_underlying <- eigen_centrality(g, directed = FALSE)$vector
   #g_eigen_c_underlying_label <- V(g)$label[g_eigen_c_underlying==max(g_eigen_c_underlying)]
-  
+
   #C Closeness centrality (harmonic if disconnected)
   #print('Finding closeness centrality')
   #g_close_c <- closeness(g, mode = 'all')
@@ -89,22 +89,22 @@ graph_main_analysis <- function(g){
   #g_cen_close_n <- harmonic_centrality(g, mode = 'all', normalized = TRUE)
   #}
   #g_close_c_label <- V(g)$label[g_close_c==max(g_close_c)]
-  
-  
+
+
   #C In-Closeness centrality
   #g_close_c_in <- closeness(g, mode = 'in')
   #g_close_c_in_n <- closeness(g, mode = 'in', normalized = TRUE)
   #g_close_c_in_label <- V(g)$label[g_close_c_in==max(g_close_c_in)]
-  
+
   #C Betweenness centrality
   #print('Finding betweenness centrality')
   #g_between_c <- betweenness(g, directed = TRUE)
   #g_between_c_label <- V(g)$label[g_between_c==max(g_between_c)]
-  
+
   #C Underlying betweenness centrality
   #g_between_c_underlying <- betweenness(g, directed = FALSE)
   #g_between_c_underlying_label <- V(g)$label[g_between_underlying_c==max(g_between_c_underlying)]
-  
+
   #C Katz centrality
   #print('Finding Katz centrality')
   #if (g_con != 'strong') {
@@ -115,15 +115,15 @@ graph_main_analysis <- function(g){
   #g_katz_c <- alpha_centrality(g, alpha = 0.5, exo=b)
   #g_katz_c_n <- g_katz_c/max(g_katz_c)
   #g_katz_c_label <- V(g)$label[g_katz_c==max(g_katz_c)]
-  
+
   #C PageRank centrality
   #print('Finding PageRank centrality')
   #g_page_c <- page_rank(g, algo = 'prpack', directed = TRUE)$vector
   #g_page_c_n <- g_cen_pagevector/max(g_cen_pagevector)
   #g_page_c_label <- V(g)$label[g_page_c==max(g_page_c)]
-  
+
   #C Underlying PageRank centrality
-  
+
   #C Return
   res <- list(
     'order' = g_order,
@@ -155,19 +155,19 @@ bfs_subgraph <- function(graph, scale) {
   # Get starting vertex
   indegree_centrality <- degree(graph, mode = "in")
   start_vertex <- which.max(indegree_centrality)
-  
+
   # Define start conditions
   target_size <- ceiling(vcount(graph) * scale)
   visited_nodes <- c()
   queue <- c(start_vertex)
-  
+
   # BFS
   while (length(visited_nodes) < target_size) {
     if (length(queue) > 0) {
       # Fetch next node from cue
       current_node <- queue[1]
       queue <- queue[-1]
-      
+
       # Check if it was visited and find neighbors and add to the queue
       if (!(current_node %in% visited_nodes)) {
         visited_nodes <- c(visited_nodes, current_node)
@@ -185,7 +185,7 @@ bfs_subgraph <- function(graph, scale) {
       possible_nodes <- indegree_centrality[unvisited_nodes]
       current_node <- unvisited_nodes[which.max(possible_nodes)]
       visited_nodes <- c(visited_nodes, current_node)
-      
+
       # Find neighbors and to the queue
       neighbors <- neighbors(graph, current_node)
       for (neighbor in neighbors) {
@@ -194,9 +194,9 @@ bfs_subgraph <- function(graph, scale) {
         }
       }
     }
-    
+
   }
-  
+
   # Create a subgraph with the visited nodes
   subgraph <- induced_subgraph(graph, visited_nodes)
   return(subgraph)
@@ -213,22 +213,65 @@ create_graph_visualization <- function(graph, layout, node_color) {
   max_degree <- max(degrees)
   alpha_values <- (degrees - min_degree) / (max_degree - min_degree)  # Normalize degrees to [0, 1]
   alpha_values <- 0.5 + 0.5 * alpha_values  # Scale to [0.25, 0.75]
-  
+
   # Calculate sizes logarithmically scaled according to degree
   sizes <- log(degrees + 1)  # Adding 1 to avoid log(0)
   min_size <- min(sizes)
   max_size <- max(sizes)
   sizes <- 1 + 4 * ((sizes - min_size) / (max_size - min_size))  # Scale sizes to [1, 5]
-  
+
   # Convert igraph to ggraph object
   g <- ggraph(graph, layout = layout) +
     geom_edge_link(edge_alpha = 0.5, edge_colour="#A0A0A0") +  # Draw edges
-    geom_node_point(aes(size = sizes, fill = node_color, alpha = alpha_values), 
+    geom_node_point(aes(size = sizes, fill = node_color, alpha = alpha_values),
                     shape = 21, color = "black", stroke = 0.5, show.legend = FALSE) +  # Draw nodes with size, fill, alpha, and black outline
     scale_size_continuous(range = c(1, 5)) +  # Set size range
     scale_fill_identity() +  # Use identity scale for fill colors
     scale_alpha_continuous(range = c(0.5, 1)) +  # Set alpha range
     theme_void()  # Remove axes and labels
-  
+
   return(g)
+}
+check_append_missing_nodes <- function(edge_tbl, node_tbl) {
+  if (all(edge_tbl$source %in% node_tbl$id) && all(edge_tbl$target %in% node_tbl$id)) {
+    print("All vertex names have been found.")
+  } else {
+    print("Some vertex names are missing.")
+    if (!all(edge_tbl$source %in% node_tbl$id)) {
+      print("Missing source nodes")
+      missing_value <- setdiff(edge_tbl$source, node_tbl$id)
+      print(missing_value)
+    }
+    if (!all(edge_tbl$target %in% node_tbl$id)) {
+      print("Missing target nodes")
+      missing_value <- setdiff(edge_tbl$target, node_tbl$id)
+      print(missing_value)
+    }
+    match_index <- which(
+      str_detect(tolower(node_tbl$id), fixed(tolower(missing_value))) |
+        str_detect(tolower(missing_value), fixed(tolower(node_tbl$id)))
+    )
+    if (length(match_index)>0) {
+      print("Similar record found, overwriting by missing name")
+      print(paste("Overwritten:",node_tbl$id[match_index[1]]))
+      print(paste("New value:",missing_value))
+      node_tbl$id[match_index[1]] <- missing_value
+    } else {
+      print("No similar record found, appending missing record.")
+      node_tbl <- rbind(node_tbl, data.frame(id = missing_value, username = missing_value))
+    }
+  }
+  return(node_tbl)
+}
+read_tsv_files <- function(folder_path) {
+  # List all .tsv files in the specified folder
+  tsv_files <- list.files(path = folder_path, pattern = "\\.tsv$", full.names = TRUE)
+
+  # Read each .tsv file and store it in a list of dataframes
+  list_of_dfs <- lapply(tsv_files, read.delim)
+
+  # Combine all dataframes into one
+  combined_df <- do.call(rbind, list_of_dfs)
+
+  return(combined_df)
 }
